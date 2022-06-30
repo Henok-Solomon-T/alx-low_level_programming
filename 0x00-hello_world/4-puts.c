28ca2007e3d096ef6f204b4bed0c3a4ad8faed6c
Henok-Solomon-T <noksol03@gmail.com>
2022-06-30 14:54:01 -0700

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
-
 /**
  *main - prints "Programming is like building a multilingual puzzle, followed by a new line",
  *Return : 0 if exited properly, otherwise non-zero
  */
-int main(void)
+int main(void) /**
+		 *main - prints "Programming is like building a multilingual puzzle, followed by a new line",
+		 */
 {
-	puts("\"Programming islike building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
