7196701a366ef56ac3cb397e045d4bf4a9d90fbd
Henok-Solomon-T <noksol03@gmail.com>
2022-06-30 15:03:49 -0700

    Hot Fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  *main- prints Programming is like building a multilingual puzzle, followed by a new line,
  *Return : if exited properly, otherwise non-zero
  */
-int main(void) {
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
