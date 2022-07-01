006b34dbbceead5ce8111a9cff2cfce21808aeee
Henok-Solomon-T <noksol03@gmail.com>
2022-07-01 14:13:59 -0700

    a program that prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*main- prints the number 00 to 99
+*Return: 0 if success else nonzero
+*/
+
+int main(void)
+{
+int x, y;
+for (x = 48; x <= 57; x++)
+{
+for (y = 48; y <= 57; y++)
+{
+{
+putchar(x);
+putchar(y);
+if (x != 57 || m != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
