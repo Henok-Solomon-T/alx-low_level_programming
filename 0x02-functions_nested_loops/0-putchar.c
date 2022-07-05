8b5e038ceaeb1a838691bd427cb954590f3c0de7
Henok-Solomon-T <noksol03@gmail.com>
2022-07-05 10:31:34 -0700

    a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+*main- prints Holberton,
+* followed by new line
+*Return: 0 if exist
+*/
+int main(void)
+{
+_putchar('H');
+_puchar('o');
+_puchar('l');
+_puchar('b');
+_puchar('e');
+_puchar('r');
+_puchar('t');
+_puchar('o');
+_puchar('n');
+_puchar('\n');
+
+return (0);
+}
