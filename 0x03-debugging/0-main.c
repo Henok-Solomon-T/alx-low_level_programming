bdd58cf47e7f1e1b9df338998b9e6e030af596b4
Henok-Solomon-T <noksol03@gmail.com>
2022-07-07 10:08:04 -0700

    C- Debugging multiple main function task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,43 +1,14 @@
-#include <stdlib.h>
-#include <time.h>
-#include <studio.h>
 #include "main.h"
-
 /**
- * positive_or_negative- test function
- *@i: integer to be checked
- *Return: 0
- */
-
-void positive_or_negative(int i)
-{
-
-int n;
-
-srand(time(0));
-n = i;
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-else if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-else
-printf("%d is negative\n", n);
-
-return (0);
-}
-/**
- * main- test function that prints
- * if int positive or negative;
+ * main- tests function that prints if integer is positive or negative
  * Return: 0
  */
 
 int main(void)
 {
+
 int i;
+
 i = 0;
 positive_or_negative(i);
 
