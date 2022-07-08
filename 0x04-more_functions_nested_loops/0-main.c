c82bbe766ffeec492a61e244dfd7cc53c0967b20
Henok-Solomon-T <noksol03@gmail.com>
2022-07-08 12:20:03 -0700

    a function that checks for uppercase character since Prototype: int _isupper(int c) ; Returns 1 if c is uppercase else 0

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -10,10 +10,10 @@ int main(void)
 
 char c;
 
-c = '0';
-printf("%c: %d\n", c, _isdigit(c));
+c = 'A';
+printf("%c: %d\n", c, _isupper(c));
 c = 'a';
-printf("%c: %d\n", c _is digit(c));
+printf("%c: %d\n", c, _isupper(c));
 return (0);
 
 }
