d49b7504c2b08a803aef63231b7c949964032e96
Henok-Solomon-T <noksol03@gmail.com>
2022-07-13 11:29:59 -0700

    a function that convert a string to an integer.

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _atoi-  a function that convert a string to an integer
+ * @s: String to be converted
+ * Return: The converted integer
+ */
+
+int _atoi(char *s)
+{
+
+int a, b , c, len, d, digit;
+
+a = b = c = len = d = digit = 0;
+while (s[a] == '=')
+len++;
+
+
+}
