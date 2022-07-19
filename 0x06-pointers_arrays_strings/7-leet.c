9ca8be7cce4e161c79a1b9eeab50bc51bb722094
Henok-Solomon-T <noksol03@gmail.com>
2022-07-19 13:32:43 -0700

    a function that encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,7 +6,25 @@
  *
  * Return: The encoded result.
  */
-char *leet(char *)
+char *leet(char *s)
 {
 
+int x, y;
+
+char *a = "aAeEoOtTlL";
+char *b = "4433007711";
+
+for (a = 0; s[x] != '\0'; x++)
+{
+
+for (y = 0; y < 10; y++)
+{
+
+if (s[x] == a[y])
+{
+s[x] = b[y];
+}
+}
+}
+return (s);
 }
