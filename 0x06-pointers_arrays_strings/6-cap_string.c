58d9b6b12d9b85691711ae6024cdc2623b39e75d
Henok-Solomon-T <noksol03@gmail.com>
2022-07-19 13:24:05 -0700

    a function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,7 +6,30 @@
  * Return: the modified or result string.
  */
 
-char *cap_string(char *)
+char *cap_string(char *s)
 {
 
+int x, y;
+
+char wsepa[13] = {
+' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+for (x = 0; s[x] != '\0'; x++)
+{
+if (x == 0 && s[x] >= 'a' && s[x] <= 'z')
+	s[x] = s[x] - 32;
+
+for (y = 0; y < 13; y++)
+{
+if (s[x] == wsepa[y])
+{
+if (s[x + 1] >= 'a' && s[x + 1] <= 'z')
+{
+s[x + 1] -= 32;
+}
+}
+}
+}
+
+return (s);
 }
