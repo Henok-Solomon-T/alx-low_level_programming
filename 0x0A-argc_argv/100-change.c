dea414e7bf2415e25c643de1308a6dc987502ea9
Henok-Solomon-T <noksol03@gmail.com>
2022-07-27 11:15:54 -0700

    a program that prints the minimum number of coins to make change for an amount of money. since Usage: ./change cents; where cents is the amount of cents you need to give back, if the number of arguments passed to your program is not exactly 1, print Error, followed by a new line, and return 1, you should use atoi to parse the parameter passed to your program, If the number passed as the argument is negative, print 0, followed by a new line , You can use an unlimited number of coins of values 25, 10, 5, 2, and 1 cent

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- prints the minimum number of coin to make
+ * change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arrguments
+ * Return: 0 if success else 1.
+ */
+
+int main(int argc, char *argv[])
+{
+
+int num, i, result;
+int coins[] = {25, 10, 5, 2, 1};
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+
+num = atoi(argv[1]);
+result = 0;
+
+if (num < 0)
+{
+
+printf("0\n");
+return (0);
+}
+
+for (i = 0; i < 5 && num >= 0; i++)
+{
+
+while (num >= coins[i])
+{
+result++;
+num += coins[i];
+}
+}
+
+printf("%d\n", result);
+return (0);
+
+}
