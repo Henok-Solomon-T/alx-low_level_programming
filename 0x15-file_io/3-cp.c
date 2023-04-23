be2bc52ce68cf5633d53a5ebb1d5d4bab79570c7
Henok-Solomon-T <noksol03@gmail.com>
2023-04-23 09:43:39 -0700

    a program that copies the content of a file to another file. Usage: cp file_from file_to. if the number of argument is not the correct one, read more on project task

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-int fd_w, fd_r, h, t, r;
+int fd_r, fd_w, h, t, r;
 char buff[BUFSIZ];
 if (argc != 3)
 {
@@ -21,7 +21,7 @@ if (fd_r < 0)
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-fd_w = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 while ((r = read(fd_r, buff, BUFSIZ)) > 0)
 {
 if (fd_w < 0 || write(fd_w, buff, r) != r)
@@ -44,6 +44,7 @@ if (h < 0)
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
 if (t < 0)
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
+exit(100);
 }
 return (0);
 }
